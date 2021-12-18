6004e054d424d3637cca2893deddd213c5188677
Marcelo Carvalho <carvalho.mg@gmail.com>
2021-12-18 10:09:23 +0000

    Add xunit

--- MobilityFriends.Functional.Tests/SearchMainPage.cs
+++ MobilityFriends.Functional.Tests/SearchMainPage.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace MobilityFriends.Functional.Tests
 {
-    internal class SearchMainPage
+    public class SearchMainPage
     {
     }
 }
